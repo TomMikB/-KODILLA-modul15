c94dbe006b516f70bf52c32211381906709d8979
Tomasz Biernacki <tmbiernacki@szczecin.home.pl>
2019-02-19 19:50:42 +0100

    Add class

--- script.js
+++ script.js
@@ -2,4 +2,31 @@
 
 (function(){
 
+	function Phone(brand, price, color, os) {
+		this.brand = brand;
+		this.price = price;
+		this.color = color;
+		this.os = os;
+	}
+
+	Phone.prototype.printInfo = function() {
+		console.log('The phone brand is: ' + this.brand + ', color is: ' + this.color + ' and the price is: ' + this.price + '.');
+	}
+
+	Phone.prototype.sys = function() {
+		console.log(this.brand + ' operating system is: ' + this.os + '.');
+	}
+
+	var SamsungGalaxyS6 = new Phone('Samsung', 800, 'black', 'Android');
+	var iPhone6s = new Phone('Apple', 1400, 'silver', 'iOS');
+	var OnePlusOne = new Phone('OnePlus', 1000, 'white', 'Android');
+
+	SamsungGalaxyS6.printInfo();
+	iPhone6s.printInfo();
+	OnePlusOne.printInfo();
+
+	SamsungGalaxyS6.sys();
+	iPhone6s.sys();
+	OnePlusOne.sys();
+
 })();
